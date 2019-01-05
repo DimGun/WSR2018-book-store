4339fc26bc06b68d6bde845da27abc06d9a62f5a
Dmitri Zhuravlenko <750150+DimGun@users.noreply.github.com>
2019-01-05 22:53:00 +0300

    Reformatted: set tabstop=2

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,72 +6,72 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    // Array to store books info
-    protected $books = array();
+  // Array to store books info
+  protected $books = array();
 
-    public function __construct()
-    {
-      $this->books[] = ['title' => 'Mystical island', 'anons' => 'A group of surrendes find themself on an island'];
-    }
+  public function __construct()
+  {
+    $this->books[] = ['title' => 'Mystical island', 'anons' => 'A group of surrendes find themself on an island'];
+  }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      return response()->json([
-        'status' => true,
-        'list' => $this->books
-      ]);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return response()->json([
+      'status' => true,
+      'list' => $this->books
+    ]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-      return response()->json([
-        'status' => true,
-        'book' => $this->books[$id]
-      ]);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    return response()->json([
+      'status' => true,
+      'book' => $this->books[$id]
+    ]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
